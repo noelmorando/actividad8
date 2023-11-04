77f88f393eb3634398ff7df80236cca05d39dbe4
Maria Morando <mnoel.morando@gmail.com>
2023-11-04 22:27:39 +0100

    subiendo actividad8

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('node:http')
+const app = require('./src/app')
+const { error } = require('node:console')
+require('dotenv').config()
+require('./src/config/db')
+
+const server = http.createServer(app)
+const PORT = process.env.PORT || 3000
+server.listen(PORT)
+server.on('listening', () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`)
+})
+server. on('error', () => {
+    console.log(error)
+})
---
+++ src/models/inmueble.model.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose')
+
+const InmuebleSchema = new Schema({
+    piso:String,
+    letra: String,
+    extension: Number,
+    habitaciones: Number,
+    alquilado: Boolean,
+    propietario: String,
+    contacto: String
+}, {
+    versionKey: false,
+    timestamps: true,
+    //TODO: fijarme si realmente voy a usar virtuales
+    toObject: {virtuals: true},
+    toJSON: {
+        virtuals: true,
+        transform: function(doc,ret){
+            delete ret.id
+        }
+    }
+})
+
+module.exports = model('inmueble', InmuebleSchema, 'inmuebles')
